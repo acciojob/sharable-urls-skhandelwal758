c68bfd36202586177adc1e2a883156c35ade36e6
skhandelwal758 <65276353+skhandelwal758@users.noreply.github.com>
2023-03-01 15:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+var btn = document.getElementById('button');
+btn.addEventListener('click', ()=>{
+    var name = document.getElementById("name").value;
+    var year = document.getElementById("year").value;
+    var url = document.getElementById("url");
+    var val = url.innerText;
+    urlnew = `${val}?name=${name}&year=${year}`;
+    console.log(val);
+    url.innerText = urlnew;
+
+});
